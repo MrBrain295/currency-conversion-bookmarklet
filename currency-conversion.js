5375bc807bf9059d975e1cf1dc3e8aa22750f932
Gabe <66077254+MrBrain295@users.noreply.github.com>
2024-07-09 16:34:54 -0500

    be less wordy

--- currency-conversion.js
+++ currency-conversion.js
@@ -1,6 +1,6 @@
-var base = prompt("Enter a ISO 4217 currency code to use as the base for the conversion.");
-var apiURL = "https://v6.exchangerate-api.com/v6/c337911db4952f08dc1e0e61/latest/" + base;
-var secondCurrency = prompt("Enter a ISO 4217 currency code to use as the currency being converted to.");
+var base = prompt("Enter a ISO 4217 currency code as the base.");
+var apiURL = "https://v6.exchangerate-api.com/v6/**your API key**/latest/" + base;
+var secondCurrency = prompt("Enter a ISO 4217 currency code as the target.");
 var r = await fetch(apiURL).then(response => response.json());
 var rate = r.conversion_rates[secondCurrency];
 alert(`One ${base} is worth ${rate} ${secondCurrency}.`);
