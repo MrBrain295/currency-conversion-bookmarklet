18a41275b3fd63b9d9f9c1f81c03dbd729f3c249
Gabe <66077254+MrBrain295@users.noreply.github.com>
2024-07-09 16:45:21 -0500

    make the other string pretty
    also i did prettifiy it and it just undid the other thing it did.

--- currency-conversion.js
+++ currency-conversion.js
@@ -1,6 +1,5 @@
 var base = prompt("Enter a ISO 4217 currency code as the base.");
-var apiURL =
-  "https://v6.exchangerate-api.com/v6/**your API key**/latest/" + base;
+var apiURL = `https://v6.exchangerate-api.com/v6/**your API key**/latest/${base}`;
 var secondCurrency = prompt("Enter a ISO 4217 currency code as the target.");
 var r = await fetch(apiURL).then((response) => response.json());
 var rate = r.conversion_rates[secondCurrency];
